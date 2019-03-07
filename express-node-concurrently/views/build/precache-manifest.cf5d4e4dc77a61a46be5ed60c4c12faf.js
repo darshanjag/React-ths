8f834df936de5bf70b98799581de08f0210ea1e6
darshanjag <itsdarshanjagtap@gmail.com>
2019-03-07 19:42:18 +0530

    react-node concurrently

---
+++ express-node-concurrently/views/build/precache-manifest.cf5d4e4dc77a61a46be5ed60c4c12faf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "309853b1d80c13a509d8",
+    "url": "/static/css/main.3827ca02.chunk.css"
+  },
+  {
+    "revision": "309853b1d80c13a509d8",
+    "url": "/static/js/main.309853b1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fe7b5a4cd2c486c409e5",
+    "url": "/static/js/2.fe7b5a4c.chunk.js"
+  },
+  {
+    "revision": "720866827e234359c7a9d7b4dc0397f4",
+    "url": "/index.html"
+  }
+];
