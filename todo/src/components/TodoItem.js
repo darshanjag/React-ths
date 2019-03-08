ed3e74e65a7c50d21754fbb7af46b917d5551f56
darshanjag <itsdarshanjagtap@gmail.com>
2019-03-09 00:48:26 +0530

    prop-drilling

--- todo/src/components/TodoItem.js
+++ todo/src/components/TodoItem.js
@@ -3,19 +3,20 @@ import propTypes from 'prop-types'
 
 export class TodoItem extends Component {
   getStyle = ()=>{
-    if(this.props.todo.completed){
-      return{
-        textDecoration: 'line-through'
-      }
-    }else{
-      return{
-        textDecoration: 'none'
-      }
+    return{
+      background: '#f4f4f4',
+      padding :'10x',
+      borderBottom: '1px ccc dotted',
+      textDecoration: this.props.todo.completed ? 
+      'line-through':'none'
     }
+    
+    
   }
   render() {
     return (
       <div style={this.getStyle()}>
+      <input type="checkbox" onChange={this.props.markComplete}></input>
        <p>{this.props.todo.title}</p> 
       </div>
     )
--- todo/src/components/Todos.js
+++ todo/src/components/Todos.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import propTypes from 'prop-types'
 class Todos extends Component {
+  markComplete = ()=>{
+    console.log("hello")
+  }
   render() {
     return this.props.todos.map((todo)=>(
-      <TodoItem key={todo.id}todo={todo} />
+      <TodoItem key={todo.id}todo={todo} markComplete={this.markComplete}/>
     ));
   }
 }
