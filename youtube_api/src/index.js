def11376b96b87b466d4c152cd7e655bf5364d50
darshanjag <itsdarshanjagtap@gmail.com>
2019-03-24 21:07:37 +0530

    passing down the props

--- youtube_api/src/index.js
+++ youtube_api/src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import YTSearch from "youtube-api-search";
 import SearchBar from './components/SearchBar';
 const API_KEY = "AIzaSyAB_CN30ouaEgNLLx4FgVhOz8c1wBjJedk"
-
-YTSearch({key: API_KEY, term:"elonmusk"},function(data){
-    console.log(data);
+state={
+search:YTSearch({key: API_KEY, term:"elonmusk"},function(data){
+    return(data);
   })
+}
 class App extends Component{
     render(){
+        
         return(
+            <div>
             <SearchBar />
+            <video_list_item search={this.state.search}/>
+            </div>
         )
     }
 }
