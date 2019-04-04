01e89a259325af46bafc8bb5470a775a8385c6a7
darshanjag <itsdarshanjagtap@gmail.com>
2019-04-04 22:56:47 +0530

    video list added

--- youtube_api/src/components/video_list.js
+++ youtube_api/src/components/video_list.js
@@ -1,12 +1,19 @@
 import React from "react";
-    const VideoList = ()=>{
-        const videoItems=props.videos.map((video)=>{
-            return <VideoListItem key={video.etag} video={video}/>
-        })
-        return(
-            <ul className="col-md-4 list-group">
-            {videoItems}
-            </ul>
+import VideoListItem from "./video_list_item";
+const VideoList = (props)=>{
+  
+    const videoItems = props.videos.map((video)=>{
+        return (
+        <VideoListItem 
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag} video={video} />
         )
-    }
-    export default VideoList;
+    });
+
+    return(
+       <ul className="col-md-4 list-group"> 
+            {videoItems}
+       </ul>
+     )
+}
+export default VideoList;
