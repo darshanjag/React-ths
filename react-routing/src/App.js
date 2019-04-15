089dee8d8afe69f2835619f2fb1820906283d61a
darshanjag <itsdarshanjagtap@gmail.com>
2019-04-15 17:56:19 +0530

    Link

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import  {BrowserRouter, Route, Link} from 'react-router-dom'
 import Profile from './components/Profile'
 import Home from './components/Home'
-import Posts from './components/Home'
+import Posts from './components/Posts'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      <header>
+      <Link to="/">Home</Link>
+      <Link to="/posts">Posts</Link>
+      <Link to="/profile">Profile</Link>
+
+      </header>
       <div className="App">
         <Route path="/" exact component={Home}/>
         <Route path="/posts" component={Posts}/>
