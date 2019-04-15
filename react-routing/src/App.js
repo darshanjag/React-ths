a0c5d3a52be6f5be6f25e868ea9426a1779e3e5d
darshanjag <itsdarshanjagtap@gmail.com>
2019-04-15 17:50:49 +0530

    basic routing

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  {BrowserRouter, Route, Link} from 'react-router-dom'
+import Profile from './components/Profile'
+import Home from './components/Home'
+import Posts from './components/Home'
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Route path="/" exact component={Home}/>
+        <Route path="/posts" component={Posts}/>
+        <Route path="/profile" component={Profile}/>
+       
+
       </div>
+      </BrowserRouter>
     );
   }
 }
